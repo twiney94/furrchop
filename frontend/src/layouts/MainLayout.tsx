53da434766c1a595bbbc3b292bba8981a72b67a6
Yass <61833982+twiney94@users.noreply.github.com>
2024-01-08 09:29:22 +0100

    header and main layout for search page

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,15 +1,19 @@
-import { Header } from '../components/Header';
+import { Header } from "../components/Header";
 import { Outlet } from "react-router-dom";
-
+import "./mainlayout.module.css";
 
 const MainLayout = () => {
 	return (
-		<div>
-			<Header />
-			<main><Outlet /></main>
+		<div className="flex flex-col h-screen w-screen items-center">
+			<div className="p-6">
+				<Header mode="default" />
+			</div>
+
+			<main>
+				<Outlet />
+			</main>
 		</div>
 	);
 };
-
 
 export default MainLayout;
