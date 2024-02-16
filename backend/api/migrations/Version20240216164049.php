17b553699d04e99e3c0d7a35526d1992eaa48383
Yass <61833982+twiney94@users.noreply.github.com>
2024-02-16 17:44:01 +0100

    Create Version20240216164049.php

---
+++ backend/api/migrations/Version20240216164049.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240216164049 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE booking_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE employee_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE leave_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE schedule_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE service_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE shop_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE booking (id INT NOT NULL, service_id INT DEFAULT NULL, user_id UUID DEFAULT NULL, shop_id INT DEFAULT NULL, employee_id INT NOT NULL, begin_date_time TIMESTAMP(0) WITH TIME ZONE NOT NULL, end_date_time TIMESTAMP(0) WITH TIME ZONE NOT NULL, comment VARCHAR(255) DEFAULT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_E00CEDDEED5CA9E6 ON booking (service_id)');
+        $this->addSql('CREATE INDEX IDX_E00CEDDEA76ED395 ON booking (user_id)');
+        $this->addSql('CREATE INDEX IDX_E00CEDDE4D16C4DD ON booking (shop_id)');
+        $this->addSql('CREATE INDEX IDX_E00CEDDE8C03F15C ON booking (employee_id)');
+        $this->addSql('COMMENT ON COLUMN booking.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE employee (id INT NOT NULL, shop_id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_5D9F75A14D16C4DD ON employee (shop_id)');
+        $this->addSql('CREATE TABLE leave (id INT NOT NULL, employee_id INT NOT NULL, begins_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, ends_at TIMESTAMP(0) WITH TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_9BB080D08C03F15C ON leave (employee_id)');
+        $this->addSql('COMMENT ON COLUMN leave.begins_at IS \'(DC2Type:datetimetz_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN leave.ends_at IS \'(DC2Type:datetimetz_immutable)\'');
+        $this->addSql('CREATE TABLE schedule (id INT NOT NULL, employee_id INT NOT NULL, day_of_week VARCHAR(10) NOT NULL, start_time TIME(0) WITHOUT TIME ZONE NOT NULL, end_time TIME(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_5A3811FB8C03F15C ON schedule (employee_id)');
+        $this->addSql('COMMENT ON COLUMN schedule.start_time IS \'(DC2Type:time_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN schedule.end_time IS \'(DC2Type:time_immutable)\'');
+        $this->addSql('CREATE TABLE service (id INT NOT NULL, shop_id INT DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, price DOUBLE PRECISION NOT NULL, duration INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_E19D9AD24D16C4DD ON service (shop_id)');
+        $this->addSql('CREATE TABLE shop (id INT NOT NULL, user_id UUID DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, open_hours JSON NOT NULL, open_days JSON NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_AC6A4CA25E237E06 ON shop (name)');
+        $this->addSql('CREATE INDEX IDX_AC6A4CA2A76ED395 ON shop (user_id)');
+        $this->addSql('COMMENT ON COLUMN shop.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, phone_number VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, is_verified BOOLEAN DEFAULT false NOT NULL, type BOOLEAN DEFAULT NULL, valid_kbis VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN "user".updated_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE4D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE8C03F15C FOREIGN KEY (employee_id) REFERENCES employee (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A14D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE leave ADD CONSTRAINT FK_9BB080D08C03F15C FOREIGN KEY (employee_id) REFERENCES employee (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE schedule ADD CONSTRAINT FK_5A3811FB8C03F15C FOREIGN KEY (employee_id) REFERENCES employee (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE service ADD CONSTRAINT FK_E19D9AD24D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE shop ADD CONSTRAINT FK_AC6A4CA2A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE booking_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE employee_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE leave_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE schedule_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE service_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE shop_id_seq CASCADE');
+        $this->addSql('ALTER TABLE booking DROP CONSTRAINT FK_E00CEDDEED5CA9E6');
+        $this->addSql('ALTER TABLE booking DROP CONSTRAINT FK_E00CEDDEA76ED395');
+        $this->addSql('ALTER TABLE booking DROP CONSTRAINT FK_E00CEDDE4D16C4DD');
+        $this->addSql('ALTER TABLE booking DROP CONSTRAINT FK_E00CEDDE8C03F15C');
+        $this->addSql('ALTER TABLE employee DROP CONSTRAINT FK_5D9F75A14D16C4DD');
+        $this->addSql('ALTER TABLE leave DROP CONSTRAINT FK_9BB080D08C03F15C');
+        $this->addSql('ALTER TABLE schedule DROP CONSTRAINT FK_5A3811FB8C03F15C');
+        $this->addSql('ALTER TABLE service DROP CONSTRAINT FK_E19D9AD24D16C4DD');
+        $this->addSql('ALTER TABLE shop DROP CONSTRAINT FK_AC6A4CA2A76ED395');
+        $this->addSql('DROP TABLE booking');
+        $this->addSql('DROP TABLE employee');
+        $this->addSql('DROP TABLE leave');
+        $this->addSql('DROP TABLE schedule');
+        $this->addSql('DROP TABLE service');
+        $this->addSql('DROP TABLE shop');
+        $this->addSql('DROP TABLE "user"');
+    }
+}
